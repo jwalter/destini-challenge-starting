8010d3eb96f626980e7dc0b84e14b001ee61498e
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-07-28 15:37:53 +0200

    Add story navigation to buttons (TODO 16-19)

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,8 @@
 import 'package:destini_challenge_starting/story.dart';
 
 class StoryBrain {
+  int _storyNumber = 0;
+
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -47,13 +49,11 @@ class StoryBrain {
     return _currentStory().choice2;
   }
 
-  Story _currentStory() => _storyData.first;
+  Story _currentStory() => _storyData[_storyNumber];
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
+  void nextStory(int choiceNumber) {}
 
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
